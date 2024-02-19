fa4b8c8ee8da0a7be12aeae6ee5f1c93aa66ca29
huzaifa7swag <huzaifakdb@gmail.com>
2024-02-19 18:49:23 -0500

    Delete dfsdf.py

--- dfsdf.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY') or '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'
