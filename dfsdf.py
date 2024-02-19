4ee4d8b1695680abcdf3cb1972567bfd58dcd45b
huzaifa7swag <huzaifakdb@gmail.com>
2024-02-19 18:45:33 -0500

    Update dfsdf.py

--- dfsdf.py
+++ dfsdf.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY') or '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'
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY') or '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'
